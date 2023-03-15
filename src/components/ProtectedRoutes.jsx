9c9f571ade22a3493d5e85bda8b7adbfb1876a4b
imad <imadhamdouch2@outlook.com>
2023-03-15 15:47:56 +0100

    version 2.8.9

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -4,7 +4,7 @@ import { Navigate,Outlet } from 'react-router-dom'
 function ProtectedRoutes() {
     const [isAutenticated,setIsAutenticated] = useState(localStorage.getItem('isLogged'));
   return (
-    isAutenticated === true?<Outlet/>:<Navigate to='/login'/>
+    isAutenticated !== true?<Outlet/>:<Navigate to='/login'/>
   )
 }
 
